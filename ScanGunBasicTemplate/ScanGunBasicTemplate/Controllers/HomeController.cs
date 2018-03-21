0e0c3b4f5bd09505d3af4ffb1e66d9287d1afb3c
Krunal Shaholia <Krunal.Shaholia@twipv.com>
2018-03-21 11:48:43 +0400

    asfasf
    fsafasfas

---
+++ ScanGunBasicTemplate/ScanGunBasicTemplate/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ScanGunBasicTemplate.DataAccess;
+
+namespace ScanGunBasicTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region "Global Variables"
+
+        #endregion
+
+        #region "Action Methods"
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+        #endregion
+
+        #region "Helper Function(s) "
+
+        #endregion 
+
+    }
+}
---
+++ ScanGunBasicTemplate/ScanGunBasicTemplate/DataAccess/DACommon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScanGunBasicTemplate.DataAccess
+{
+    public class DACommon
+    {
+        #region "Global Variable(s)"
+        #endregion
+
+        #region "Connection & Constructor Function(s)"
+        #endregion 
+
+
+    }
+}
